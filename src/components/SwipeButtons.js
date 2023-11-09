f9e230af77ce567f2bf82a8d5857c4afa3bb7887
ArceJC <juanarce45@cenyca.edu.mx>
2023-11-09 10:39:35 -0800

    Revisando Documentacion UseState

--- src/components/SwipeButtons.js
+++ src/components/SwipeButtons.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import "./SwipeButtons.css";
 import ReplayIcon from "@material-ui/icons/Replay";
 import CloseIcon from "@material-ui/icons/Close";
@@ -7,7 +7,19 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
 import IconButton from "@material-ui/core/IconButton";
 const SwipeButtons = () => {
+  const [rayos, setRayos] = useState([])
+  /*useEffect(() => {
+    async function fetchData() {
+    const req = await axios.get("/dating/cards")
+    setPeople(req.data)
+    }
+    fetchData()
+  },[])*/
   //Aqui va la funcion que manejara el evento de cuando presionamos un IconButton
+  const rayoPress = () => {
+    console.log("Click")
+  };
+
   return (
     <div className="swipeButtons">
       <IconButton className="swipeButtons__repeat">
@@ -22,9 +34,9 @@ const SwipeButtons = () => {
       <IconButton className="swipeButtons__right">
         <FavoriteIcon fontSize="large" />
       </IconButton>
-      <IconButton className="swipeButtons__lightning" onClick={() => {
-    alert('clicked');
-  }}>
+      <IconButton className="swipeButtons__lightning" onClick={(args) => 
+    rayoPress()
+  }>
         <FlashOnIcon fontSize="large" />
       </IconButton>
     </div>
